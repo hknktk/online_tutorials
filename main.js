b7e5013851030a2fde46acb37c5065e3386529d8
hknktk <75674444+hknktk@users.noreply.github.com>
2021-03-11 00:34:27 +0900

    commit
    更新した。

---
+++ main.js
@@ -0,0 +1,11 @@
+{
+  window.addEventListener("scroll",()=>{
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 0);
+  })
+
+  function toggle(){
+    let header = document.querySelector('header');
+    header.classList.toggle('active');
+  }
+}
